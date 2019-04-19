a4c4054b85d6a38ec2689bed6402f017bf6b1550
Kevin Dockx <kevin.dockx@gmail.com>
2019-04-19 10:34:44 +0200

    Add solution for IDP

--- Rmdb.Web.Client/Startup.cs
+++ Rmdb.Web.Client/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Rmdb.Web.Client
 {
