357d80557461d5eccf1ecc25bc795575cfe1b176
Evert De Vos <Evert.DeVos@realdolmen.com>
2019-06-13 22:26:35 +0200

    fixed tag helper

--- Rmdb.Web.Client/TagHelpers/EmailTaghelper.cs
+++ Rmdb.Web.Client/TagHelpers/EmailTaghelper.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace Rmdb.Web.Client.TagHelpers
 {
+    [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
     public class EmailTaghelper : TagHelper
     {
-
+        private const string Domain = "realdolmen.com";
+        public string MailTo { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-           
+            output.TagName = "a";
+            var address = $"{MailTo}@{Domain}";
+            output.Attributes.Add(new TagHelperAttribute("href", $"mailto:{address}"));
+            output.Content.SetContent($"{address}");
         }
     }
 }
